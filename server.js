619c8532959d2da5fbe5ac82ccf5b14472394dec
Arb3G <rbg242570@gmail.com>
2025-06-11 23:57:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 
+const express = require('express');
+const app = express();
+const buycjsRoute = require('./routes/buycjs');
+
+app.use(express.json());
+app.use('/buycjs', buycjsRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`CJS API is live on port ${PORT}`);
+});
