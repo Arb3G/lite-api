51f5d16ad5a2259fc0a46ab9eb78c4e9ed6ea2f0
Arb3G <rbg242570@gmail.com>
2025-06-12 10:10:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use('/buycjs', buycjsRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`CJS API is live on port ${PORT}`);
+  console.log(`CJS API is live on port ${process.env.PORT || 3000}`);
 });
