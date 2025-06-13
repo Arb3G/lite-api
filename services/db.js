d6ebd6fd9fa76e209923f082fb3c52f4f0dee39a
Arb3G <rbg242570@gmail.com>
2025-06-12 21:55:50 -0400

    Update db.js

--- services/db.js
+++ services/db.js
@@ -1,21 +1,20 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("../firebase-key.json");
+const admin = require('firebase-admin');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
-const usersRef = db.collection('users');
+
+async function addUser(userId, publicKey) {
+  await db.collection('users').doc(userId).set({ publicKey });
+}
 
 async function getUser(userId) {
-  const doc = await usersRef.doc(userId).get();
+  const doc = await db.collection('users').doc(userId).get();
   return doc.exists ? doc.data() : null;
 }
 
-async function addUser(userId, publicKey) {
-  await usersRef.doc(userId).set({ publicKey });
-  return true;
-}
-
-module.exports = { getUser, addUser };
+module.exports = { addUser, getUser };
