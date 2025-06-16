77b4fb3577031d5137766b3e4c9e3e2df25271a6
Arb3G <rbg242570@gmail.com>
2025-06-16 12:52:09 -0400

    Update buycjs.js

--- routes/buycjs.js
+++ routes/buycjs.js
@@ -1,7 +1,7 @@
 // buycjs.js
 
 const registration = require('./registration');
-const { buyCJS } = require('../services/buycjs');
+const { buyCJS } = require('./buycjs');
 const readline = require('readline');
 
 // Helper: Prompt user input from shell
